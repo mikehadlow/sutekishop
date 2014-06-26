88913f2ba21d5a82b2d242ab25bb379d33b0b7bf
Mike Hadlow <mike@suteki.co.uk>
2014-06-26 09:22:20 +0100

    put ViewPage.cs back.

---
+++ Suteki.Shop/Suteki.Shop/Views/ViewPage.cs
@@ -0,0 +1,22 @@
+﻿using Suteki.Shop.HtmlHelpers;
+
+namespace Suteki.Shop
+{
+    public class ViewPage<T> : MvcContrib.FluentHtml.ModelViewPage<T> where T : class
+    {
+        public ViewPage()
+            : base(new LowercaseFirstCharacterOfNameBehaviour())
+        {
+
+        }
+    }
+
+    public class ViewUserControl<T> : MvcContrib.FluentHtml.ModelViewUserControl<T> where T : class
+    {
+        public ViewUserControl()
+            : base(new LowercaseFirstCharacterOfNameBehaviour())
+        {
+
+        }
+    }
+}
