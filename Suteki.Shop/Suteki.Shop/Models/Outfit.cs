3f47f539aef97d696b071c167de80343080386d9
Mike Hadlow <mike@suteki.co.uk>
2014-07-14 11:22:26 +0100

    Added outfit model.

---
+++ Suteki.Shop/Suteki.Shop/Models/Outfit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Suteki.Common;
+using Suteki.Common.Models;
+
+namespace Suteki.Shop
+{
+    public class Outfit : IOrderable, IActivatable, IUrlNamed, INamedEntity
+    {
+        public virtual int Position { get; set; }
+        public virtual bool IsActive { get; set; }
+        public virtual string UrlName { get; set; }
+        public virtual int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public virtual string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public virtual string Description { get; set; }
+
+        IList<OutfitImage> outfitImages = new List<OutfitImage>();
+        public virtual IList<OutfitImage> OutfitImages
+        {
+            get { return outfitImages; }
+            set { outfitImages = value; }
+        }
+
+        IList<OutfitProduct> outfitProducts = new List<OutfitProduct>();
+        public virtual IList<OutfitProduct> OutfitProducts
+        {
+            get { return outfitProducts; }
+            set { outfitProducts = value; }
+        }
+    }
+}
---
+++ Suteki.Shop/Suteki.Shop/Models/OutfitProduct.cs
@@ -0,0 +1,13 @@
+﻿using Suteki.Common;
+using Suteki.Common.Models;
+
+namespace Suteki.Shop
+{
+    public class OutfitProduct : IOrderable, IEntity
+    {
+        public int Position { get; set; }
+        public int Id { get; set; }
+        public Outfit Outfit { get; set; }
+        public Product Product { get; set; }
+    }
+}
